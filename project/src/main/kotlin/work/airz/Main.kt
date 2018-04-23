2b8870b2db8e7231c43b6a14c895ceb7699f0c66
khrom <devs@outlook.jp>
2018-04-23 19:35:52 +0900

    add 動画内のライブ認識　やってみた

--- project/src/main/kotlin/work/airz/Main.kt
+++ project/src/main/kotlin/work/airz/Main.kt
@@ -6,6 +6,7 @@ import org.bytedeco.javacpp.opencv_imgcodecs.imwrite
 import org.bytedeco.javacv.OpenCVFrameConverter
 import org.bytedeco.javacv.Java2DFrameConverter
 import org.bytedeco.javacv.OpenCVFrameGrabber
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
@@ -30,15 +31,21 @@ fun main(args: Array<String>) {
     val ffmprobe = FFprobe(ffprobeExec.absolutePath)
     val format = ffmprobe.probe(input.absolutePath)
     val videoFormat = format.streams[0]
+    val videoFrameRate = videoFormat.r_frame_rate.toDouble()
 
 
-    println("フレームレート ${videoFormat.r_frame_rate.toDouble()}")
-//レディーアクション　開始フレーム　4000 5559700559225408535
-    while (grabber.frameNumber < grabber.lengthInFrames) {
+    println("フレームレート ${videoFrameRate}")
+    var whiteCount = 0
+    var frameCount = 0
+    var startFrame = 0
+    var stopFrame = 0
+    var intervalList: List<Double>? = null
+
+    while (grabber.frameNumber < grabber.lengthInFrames && stopFrame == 0) {
         val grabbed = grabber.grab() ?: break
         if (grabbed.image == null) continue
+//        if (grabber.frameNumber % 2 == 1) continue //負荷削減
         val image = converter.convert(grabbed)
-//        if (ImageSearch().isSameImage(ImageSearch().getVector(image), 2, 5559700559225408535L)) {
         /**
          *
          * プリ
@@ -46,16 +53,52 @@ fun main(args: Array<String>) {
          *
          * 曲ごとの秒数
          * レディー・アクション            120秒　赤ステージ 開始        5559700559225408535
-         * Let'sプリ☆チャン              143秒　町        開始        1122503488345012281
+         * Let'sプリ☆チャン              143秒　町        開始        -1122503488345012281
          * キラキラプリ☆チャンワールド     127秒　町
          * ワン・ツー・スウィーツ          119秒　花舞台     開始        3978760074848754912
+         *
+         * 最後に白フレームが5~10フレームくらい入る
          */
-//        println("同じのあった")
-        imwrite("${String.format("%06d", grabber.frameNumber)}_${String.format("%d", ImageSearch().getVector(image))}.png", converterToMat.convert(grabbed))
-//        calcTime(grabber.frameNumber, videoFormat.r_frame_rate.toDouble())//フレームレートが多分おかしい
-//        println("フレーム数" + grabber.frameNumber)
-//        }
 
+        if (startFrame == 0) intervalList = getTarget(image, videoFrameRate)
+
+        if (intervalList != null && startFrame != 0) {
+            intervalList.forEach {
+                if (grabber.frameNumber - startFrame + videoFrameRate * 5 > it && ImageSearch().isSameImage(ImageSearch().getVector(image), 0, 0L)) {
+                    //白フレームに入ったか確認
+                    whiteCount++
+                    if (whiteCount > 5) {
+                        calcTime(grabber.frameNumber, videoFrameRate)
+                        stopFrame = grabber.frameNumber
+                        println("継続時間" + (calcTime(stopFrame, videoFrameRate) - calcTime(startFrame, videoFrameRate)))
+                    }
+                }
+            }
+        }
+
+        if (intervalList != null && startFrame == 0) {//フレームカウントの開始
+            println("frame hash${ImageSearch().getVector(image)}")
+            calcTime(grabber.frameNumber, videoFrameRate)//フレームレートが多分おかしいのでffmpegからとってる
+            startFrame = grabber.frameNumber
+            imwrite("${String.format("%06d", grabber.frameNumber)}_${String.format("%d", ImageSearch().getVector(image))}.png", converterToMat.convert(grabbed))
+            println("フレーム数" + grabber.frameNumber)
+        }
+
+        if (grabber.frameNumber % 100 == 0) calcTime(grabber.frameNumber, videoFrameRate)
+    }
+}
+
+/**
+ * ターゲットだった場合に曲の秒数リストを返す。
+ * (会場ごとに複数の曲が存在する
+ */
+fun getTarget(image: BufferedImage, frameRate: Double): List<Double>? {
+    val interValMap = mapOf(Pair(5559700559225408535L, listOf(120 * frameRate)), Pair(-1122503488345012281L, listOf(143 * frameRate, 127 * frameRate)), Pair(3978760074848754912L, listOf(119 * frameRate)))
+    val hash = interValMap.keys.firstOrNull { ImageSearch().isSameImage(ImageSearch().getVector(image), 2, it) }
+    return if (hash != null) {
+        interValMap[hash]
+    } else {
+        null
     }
 }
 

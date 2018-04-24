ee18a930d03c5dcb79b566043a1946cd9642f0fc
khrom <devs@outlook.jp>
2018-04-25 00:16:43 +0900

    add 動画の回転エンコード　やってみた

--- project/src/main/kotlin/work/airz/Main.kt
+++ project/src/main/kotlin/work/airz/Main.kt
@@ -1,18 +1,106 @@
 package work.airz
 
+import net.bramp.ffmpeg.FFmpeg
+import net.bramp.ffmpeg.FFmpegExecutor
 import net.bramp.ffmpeg.FFprobe
-import org.bytedeco.javacpp.*
+import net.bramp.ffmpeg.builder.FFmpegBuilder
 import org.bytedeco.javacpp.opencv_imgcodecs.imwrite
 import org.bytedeco.javacv.OpenCVFrameConverter
 import org.bytedeco.javacv.Java2DFrameConverter
 import org.bytedeco.javacv.OpenCVFrameGrabber
 import java.awt.image.BufferedImage
 import java.io.File
-import javax.imageio.ImageIO
 
 
 fun main(args: Array<String>) {
-//    println(ImageSearch().getVector(ImageIO.read(File("./open.png"))))
+    if (args.size < 2) {
+        println("usage")
+        println("java -jar /source/path/mp4 ...  /dest/path/")
+        System.exit(1)
+    }
+    for (index in 0 until args.size - 1) {
+        if (File(args[index]).isDirectory || !File(args[index]).exists()) {
+            println("入力が不正")
+            System.exit(1)
+        }
+    }
+    if (!File(args.last()).isDirectory) {
+        println("出力先が不正")
+        System.exit(1)
+    }
+    for (index in 0 until args.size - 1) {
+        encodeFile(File(args[index]), File(args.last()))
+    }
+
+}
+
+fun encodeFile(input: File, dest: File) {
+    val currentDir = System.getProperty("user.dir")
+    val ffmpegExec = File(currentDir + File.separator + "encoder", getExtByPlatform("ffmpeg"))
+    val ffprobeExec = File(currentDir + File.separator + "encoder", getExtByPlatform("ffprobe"))
+    if (!ffmpegExec.exists() || !ffprobeExec.exists()) {
+        println("encoder does not exist!")
+    }
+
+    val ffmpeg = FFmpeg(ffmpegExec.absolutePath)
+    val ffmprobe = FFprobe(ffprobeExec.absolutePath)
+
+    /**
+     * formatについて
+     * streams[0]がビデオ,streams[1]がオーディオ
+     */
+    val format = ffmprobe.probe(input.absolutePath)
+    val videoFormat = format.streams[0]
+    val audioFormat = format.streams[1]
+
+//    //now encoding
+    val ffmpegBuilder = FFmpegBuilder()
+            .setInput(input.absolutePath)
+            .overrideOutputFiles(true)
+            .addOutput(File(dest.absolutePath, "${input.nameWithoutExtension}-rotate.${input.extension}").absolutePath)
+            .setAudioChannels(audioFormat.channels)
+            .setAudioCodec(audioFormat.codec_name)        // using the aac codec
+            .setAudioSampleRate(audioFormat.sample_rate)  // at 48KHz
+            .setAudioBitRate(audioFormat.bit_rate)      // at 32 kbit/s
+
+            .setVideoCodec("libx264")     // Video using x264
+            .setVideoFrameRate(videoFormat.r_frame_rate.numerator, videoFormat.r_frame_rate.denominator)     // at 24 frames per second
+            .setVideoBitRate(videoFormat.bit_rate)
+            .setVideoResolution(1080, 1920) // at 640x480 resolution
+            .setFormat("mp4")
+            .addExtraArgs("-vf", "transpose=2")
+//            .setVideoQuality(16.0)
+            .done()
+
+    val executor = FFmpegExecutor(ffmpeg, ffmprobe)
+    val job = executor.createTwoPassJob(ffmpegBuilder)
+    println("start encoding")
+    job.run()
+    println("finish")
+}
+
+/**
+ * ターゲットだった場合に曲の秒数リストを返す。
+ * (会場ごとに複数の曲が存在する
+ */
+fun getTarget(image: BufferedImage, frameRate: Double): List<Double>? {
+    val interValMap = mapOf(Pair(5559700559225408535L, listOf(120 * frameRate)), Pair(-1122503488345012281L, listOf(143 * frameRate, 127 * frameRate)), Pair(3978760074848754912L, listOf(119 * frameRate)))
+    val hash = interValMap.keys.firstOrNull { ImageSearch().isSameImage(ImageSearch().getVector(image), 2, it) }
+    return if (hash != null) {
+        interValMap[hash]
+    } else {
+        null
+    }
+}
+
+fun calcTime(frame: Int, frameRate: Double): Double {
+    val time = frame / frameRate
+    println("時間  ${String.format("%02d", (time / 60).toInt())}:${String.format("%05.2f", time % 60)}")
+    return time //なんフレーム目か分かれば求まる
+}
+
+fun analyze() {
+    //    println(ImageSearch().getVector(ImageIO.read(File("./open.png"))))
     var converterToMat = OpenCVFrameConverter.ToMat()
     var converter = Java2DFrameConverter()
     val input = File("キラキラ.mp4")
@@ -36,7 +124,6 @@ fun main(args: Array<String>) {
 
     println("フレームレート ${videoFrameRate}")
     var whiteCount = 0
-    var frameCount = 0
     var startFrame = 0
     var stopFrame = 0
     var intervalList: List<Double>? = null
@@ -64,6 +151,7 @@ fun main(args: Array<String>) {
 
         if (intervalList != null && startFrame != 0) {
             intervalList.forEach {
+                //フレームレート*5を足しているのは秒間調整のため
                 if (grabber.frameNumber - startFrame + videoFrameRate * 5 > it && ImageSearch().isSameImage(ImageSearch().getVector(image), 0, 0L)) {
                     //白フレームに入ったか確認
                     whiteCount++
@@ -85,46 +173,5 @@ fun main(args: Array<String>) {
         }
 
         if (grabber.frameNumber % 100 == 0) calcTime(grabber.frameNumber, videoFrameRate)
-    }
-}
-
-/**
- * ターゲットだった場合に曲の秒数リストを返す。
- * (会場ごとに複数の曲が存在する
- */
-fun getTarget(image: BufferedImage, frameRate: Double): List<Double>? {
-    val interValMap = mapOf(Pair(5559700559225408535L, listOf(120 * frameRate)), Pair(-1122503488345012281L, listOf(143 * frameRate, 127 * frameRate)), Pair(3978760074848754912L, listOf(119 * frameRate)))
-    val hash = interValMap.keys.firstOrNull { ImageSearch().isSameImage(ImageSearch().getVector(image), 2, it) }
-    return if (hash != null) {
-        interValMap[hash]
-    } else {
-        null
-    }
-}
-
-fun calcTime(frame: Int, frameRate: Double): Double {
-    val time = frame / frameRate
-    println("時間  ${String.format("%02d", (time / 60).toInt())}:${String.format("%05.2f", time % 60)}")
-    return time //なんフレーム目か分かれば求まる
-}
-
-fun convert() {
-    var converterToMat = OpenCVFrameConverter.ToMat()
-    var converter = Java2DFrameConverter()
-    var grabber = OpenCVFrameGrabber.createDefault(File("output.mp4"))
-    grabber.start()
-
-    var videoMat: opencv_core.Mat
-    var count = 0
-    while (grabber.frameNumber < grabber.lengthInFrames) {
-        val grabbed = grabber.grab() ?: break
-        val image = converter.convert(grabbed)
-        if (ImageSearch().getVector(image) == -8887692564028994481L) {
-            println("同じの会った")
-            println(grabbed.timestamp)
-            imwrite("${String.format("%06d", count)}_${String.format("%06d", ImageSearch().getVector(image))}.png", converterToMat.convert(grabbed))
-            count++
-        }
-
     }
 }
